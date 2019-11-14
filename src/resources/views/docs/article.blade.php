a02c2326c856da7f578ffd400db2ce67d6202bd6
Piotr <pio.skro@gmail.com>
2019-11-14 11:36:32 +0100

    SCSS

--- src/resources/views/docs/article.blade.php
+++ src/resources/views/docs/article.blade.php
@@ -1,13 +1,8 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSRF Token -->
     <title>Documentation</title>
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
     <link href="{{ asset('css/docs.css') }}" rel="stylesheet">
 </head>
